88c1fc083bdbbdd6594a874cf282702993b8e577
AnPham <anphambk@gmail.com>
2017-06-23 02:43:45 +0800

    prevent printing log to monitor

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,7 @@ int sc_main(int argc, char **argv)
 {
     char *verb;
     char *log_file;
+    int show_log;
 
     map<string,sc_verbosity> parser;
     parser["SC_NONE"]=SC_NONE;
@@ -15,10 +16,14 @@ int sc_main(int argc, char **argv)
     parser["SC_DEBUG"]=SC_DEBUG;
 
     log_file  = sknobs_get_string("log_file", "sc.log");
+    show_log = sknobs_get_value("show_log", 1);
     sc_report_handler::set_log_file_name(log_file);
     verb = sknobs_get_string("verbosity", "SC_LOW");
     string s = string(verb);
     sc_report_handler::set_verbosity_level(parser[s]);
+    if(show_log == 0) {
+        sc_report_handler::set_actions(SC_INFO, SC_LOG);
+    }
 
     SC_REPORT_INFO_VERB("main", "Building .....", SC_FULL);
     top t("top");
